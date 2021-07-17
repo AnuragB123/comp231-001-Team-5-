5e19430b2c70fe9491b411636d52a5824b3b673c
AnuragB123 <52175974+AnuragB123@users.noreply.github.com>
2021-07-17 11:28:21 -0400

    Add files via upload
    Account Information Page - Framework - still some work to be done

---
+++ accountInformation.js
@@ -0,0 +1,13 @@
+function editAccount() {
+    var editAccount = document.getElementById("editForm");
+    if (editAccount.style.display === "none")
+    {
+        editAccount.style.display = "block";
+    } else {
+        editAccount.style.display = "none";
+    }
+}
+
+function updateAccount() {
+    //Update the account's username and password - yet to figure out
+}
