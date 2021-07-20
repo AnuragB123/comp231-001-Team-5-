6049a79d1634df833388f6267cd097d57989222a
AnuragB123 <52175974+AnuragB123@users.noreply.github.com>
2021-07-20 19:18:57 -0400

    Add files via upload
    Fixed the functionality of the page

--- checklist.js
+++ checklist.js
@@ -1,35 +1,4 @@
- // Create a close button and append it to each list item
- var myList = document.getElementsByTagName("LI");
- var i;
- for (i = 0; i < myList.length; i++)
- {
-     var span = document.createElement("SPAN");
-     var txt = document.createTextNode("\u00D7");
-     span.className = "close";
-     span.appendChild(txt);
-     myList[i].appendChild(span);
- }
- 
- //Click on close button to hide/remove current list item
- var close = document.getElementsByClassName("close");
- var i;
- for (i = 0; i < close.length; i++)
- {
-     close[i].onclick = function() {
-         var div = this.parentElement;
-         div.style.display = "none";
-     }
- }
- 
- // Add a checked symbol when clicked on a list item
- var list = document.querySelector('ul');
- list.addEventListener('click', function(ev) {
-     if (ev.target.tagName === 'LI') {
-         ev.target.classList.toggle('checked');
-     }
- }, false);
- 
- // Create a new list item when adding an item to To Do List
+// Create a new list item when adding an item to To Do List
  function newElementToDo() {
      var li = document.createElement("li");
      var inputValue = document.getElementById("myInput").value;
