6ef5a38d3048279e08695ef07cf43ff4b3f03151
AnuragB123 <52175974+AnuragB123@users.noreply.github.com>
2021-07-28 10:00:17 -0400

    Add files via upload
    Fixed the Pages

--- accountInformation.js
+++ accountInformation.js
@@ -1,20 +1,20 @@
+//Update Profile Information
 function update_Profile()
 {
         document.getElementById("h_name").innerHTML = document.getElementById("fullName").value;
         document.getElementById("h_email").innerHTML = document.getElementById("email").value;
-        document.getElementById("h_phone").innerHTML = document.getElementById("phone").value;
-        document.getElementById("h_status").innerHTML = document.getElementById("status").value;
+        document.getElementById("h_password").innerHTML = document.getElementById("password").value;
+        document.getElementById("h_userType").innerHTML = document.getElementById("userType").value;
 }    
+
+//Sign Out the User, Goes back to the Sign In Page
 function signOut()
-    {
+{
       window.location = "signIn.html"; // Redirecting to other page.
-      return false;
-    }
+}
+
+//Cancels the editing and goes back to Home Screen
 function cancel()
-    {
-        document.getElementById("fullName").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("phone").value = "";
-        document.getElementById("status").value = "";
-        return false;
-    }
+{
+    window.location = "homeScreen.html"; // Redirecting to other page.
+}
