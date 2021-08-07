7579c6064bfd9e81d6f917f8de3b905be8830870
301019618 <saaimatul@gmail.com>
2021-08-07 11:21:59 -0400

    navbar fixed for grader

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports =
 {
-   "URI": "mongodb+srv://ShoumikHoque:XCMFPF4KEjRUlY7A@cluster0.k543y.mongodb.net/mern?retryWrites=true&w=majority"
+   "URI": "mongodb+srv://teamSoftware:6s9uSS3h-ts!sxG@teamcluster.fbqqd.mongodb.net/studyApp?retryWrites=true&w=majority"
 }
